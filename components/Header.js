2d5fcc88f285e11ba349f3aa356332cb9a53eb92
anagdinizp <anagabriela0399@hotmail.com>
2022-12-29 09:50:23 -0300

    Implementando funcionalidade de date&calendar picker

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,30 @@
 import Image from "next/image";
+import { useState } from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
 
 export default function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [nOfGuests, setNoOfGuests] = useState(1);
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  function resetInput() {
+    setSearchInput("");
+  }
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left - airbnb logo*/}
@@ -17,6 +41,8 @@ export default function Header() {
       <div className="flex items-center md:border-2 rounded-full py-2">
         <input
           type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           placeholder="Start your search"
           className="bg-transparent flex-grow outline-none pl-5 md:shadow-sm text-sm text-gray-400"
         />
@@ -82,6 +108,49 @@ export default function Header() {
           </svg>
         </div>
       </div>
+
+      {/* calendar and guests */}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          {/* calendar */}
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+
+          {/* guests */}
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="h-5"
+            >
+              <path d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z" />
+            </svg>
+            <input
+              type="number"
+              value={setNoOfGuests}
+              min={1}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              className="w-12 pl2 text-lg outline-none text-red-400"
+            />
+          </div>
+
+          {/* buttons */}
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
