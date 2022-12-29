e6bde6a8b02031d24f8578b0124aa3dfb7d903e8
anagdinizp <anagabriela0399@hotmail.com>
2022-12-28 23:02:14 -0300

    Concluindo o footer

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+export default function Footer() {
+  return (
+    <div className="grid grid-cols-1 gap-y-10 px-32 justify-items-center py-14 bg-gray-100 text-gray-600 sm:grid-cols-2 md:grid-cols-4">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Tailwind is amazing</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Next.js app</p>
+        <p>I am a junior dev</p>
+        <p>Lets learn together</p>
+        <p>My Github anagdinizp</p>
+        <p>My LinkedIn anagdinizp</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi Youtube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </div>
+  );
+}
